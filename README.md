41b64d04cfe2c713f5a3a201f6d703069da93978
artawood <artawood.gmail.com>
2019-07-19 05:03:58 -0700

    successful implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-dnd
+
 Test project for react dnd
+
+Github `https://github.com/atlassian/react-beautiful-dnd`
+
+Free Video Tutorial at `https://egghead.io/lessons/react-customize-screen-reader-messages-for-drag-and-drop-with-react-beautiful-dnd`
+
+Sample sandbox code `https://codesandbox.io/s/9z7qwmmr7r`
---
+++ src/task.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${props => (props.isDragging ? "lightblue" : "white")};
+  display: flex;
+`;
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
+`;
+
+export default class Task extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            innerRef={provided.innerRef}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            <Handle {...provided.dragHandleProps} />
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
